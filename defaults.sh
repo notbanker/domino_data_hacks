b43f52782a4127b46b4c17051b7283219e0c78d3
notbanker <peter.cotton@gmail.com>
2016-06-13 09:07:07 -0400

    Create defaults.sh

---
+++ defaults.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Example of setting defaults without using Domino env. variables explicitly
+
+if [[ $OSTYPE == *"linux" ]]      # Running on Domino, refine you use linux at home ;)
+then
+    default_project="/mnt/${USER}/larry"   
+    default_size=full
+else
+    default_project="/Users/${USER}/project/larry"   # Running on local
+    default_size=small
+fi
+
+project=${1:-default_project}
+sz=${2:-${default_sz}}
