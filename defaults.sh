69015840427cacc48826f3b3c5d81d856386f44f
notbanker <peter.cotton@gmail.com>
2016-06-13 09:43:07 -0400

    Update defaults.sh

--- defaults.sh
+++ defaults.sh
@@ -3,13 +3,13 @@
 # Example of setting defaults without using Domino env. variables explicitly
 
 USER=$(whoami)
-if [[ $OSTYPE == *"linux" ]]      # Running on Domino, refine you use linux at home ;)
+if [[ $OSTYPE == *"darwin" ]]      # Running on local - adjust as needed
 then
+    default_project="/Users/${USER}/project/larry"   
+    default_size=small
+else
     default_project="/mnt/${USER}/larry"   
     default_size=full
-else
-    default_project="/Users/${USER}/project/larry"   # Running on local
-    default_size=small
 fi
 
 project=${1:-default_project}
