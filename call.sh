04967b08e9ebe6b6d6c29a5c77fe160aba5522d5
notbanker <peter.cotton@gmail.com>
2016-06-17 09:27:14 -0400

    Update call.sh

--- call.sh
+++ call.sh
@@ -14,10 +14,10 @@ arg2=${3}
 # Send request to start job to domino
 temporary_response_file="response_.txt"
 curl -X POST \
-https://api.dominodatalab.com/v1/projects/n581512/larry/runs \
--H 'X-Domino-Api-Key: 7DunjmdG3h1BZCTZVoc8eKaVQnWVn3m4dUinvEQqRoCzMAYeqR27LVMSUbWKVgzF' \
+https://api.dominodatalab.com/v1/projects/USER/PROJECT/runs \
+-H 'X-Domino-Api-Key: 7DunTrump1BcouldCnTZVhitcaKBarnQDooRwithAWrifle' \
 -H "Content-Type: application/json" \
--d '{"command": ["'"${cmd}"'", "/mnt/n581512", "'"${arg1}"'", "'"${arg2}"'"], "isDirect": false}' > ${temporary_response_file}
+-d '{"command": ["'"${cmd}"'", "'"${arg1}"'", "'"${arg2}"'"], "isDirect": false}' > ${temporary_response_file}
 echo "Sent command to start jobs with arg1 ${arg1} and arg2 ${arg2}."
 runId_quoted=$(grep -oE '"runId":"(.*)",' ${temporary_response_file} | cut -d: -f2)
 runId=${runId_quoted:1:${#runId_quoted}-3}
@@ -29,8 +29,8 @@ while true;
 do
     sleep 60s
     echo "Polling ..."
-    response=$(curl https://api.dominodatalab.com/v1/projects/n581512/larry/runs/$runId \
-    -H 'X-Domino-Api-Key: 7DunjmdG3h1BZCTZVoc8eKaVQnWVn3m4dUinvEQqRoCzMAYeqR27LVMSUbWKVgzF' | grep -oE '"isCompleted":.*')
+    response=$(curl https://api.dominodatalab.com/v1/projects/USER/PROJECT/runs/$runId \
+    -H 'X-Domino-Api-Key: 7DuVOtdgeforzhimFanDitYouWEFfault' | grep -oE '"isCompleted":.*')
     if [[ ${response} == *true* ]]
     then
         echo "Job $runId has finished"
